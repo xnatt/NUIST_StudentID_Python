fd0cae0d4e4e96e9cd6be2ffd1e45427a90b4562
xnatt <2806058107@qq.com>
2025-03-23 13:11:57 +0800

    添加 MultiplyThreeNums.py 程序，用于计算三个数字的乘积

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,6 @@
+num1 = float(input("first number: "))
+num2 = float(input("second number: "))
+num3 = float(input("third number: "))
+
+product = num1 * num2 * num3
+print(f"the product of three numbers: {product}")
